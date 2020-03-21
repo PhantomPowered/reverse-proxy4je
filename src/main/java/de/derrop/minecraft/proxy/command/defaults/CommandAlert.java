be3f0d48e40f3fb6b84231a88b8d327df34ae8ad
derrop <derrop@outlook.de>
2020-03-21 16:53:45 +0100

    Remove duplicate ChatColor class

--- src/main/java/de/derrop/minecraft/proxy/command/defaults/CommandAlert.java
+++ src/main/java/de/derrop/minecraft/proxy/command/defaults/CommandAlert.java
@@ -1,10 +1,10 @@
 package de.derrop.minecraft.proxy.command.defaults;
 
-import de.derrop.minecraft.proxy.ChatColor;
 import de.derrop.minecraft.proxy.MCProxy;
 import de.derrop.minecraft.proxy.command.Command;
 import de.derrop.minecraft.proxy.command.CommandSender;
 import de.derrop.minecraft.proxy.connection.ConnectedProxyClient;
+import net.md_5.bungee.api.ChatColor;
 
 public class CommandAlert extends Command {
 
