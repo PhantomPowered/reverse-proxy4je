bbd04b5434dd6695d423f53373bc70eda4794828
derrop <derrop@outlook.de>
2020-06-24 02:16:39 +0200

    Fix compile error

--- proxy-plugins/gommecw-stream/src/main/java/com/github/derrop/proxy/plugins/gommecw/labyconnect/user/ChatRequest.java
+++ proxy-plugins/gommecw-stream/src/main/java/com/github/derrop/proxy/plugins/gommecw/labyconnect/user/ChatRequest.java
@@ -2,10 +2,9 @@ package com.github.derrop.proxy.plugins.gommecw.labyconnect.user;
 
 import com.mojang.authlib.GameProfile;
 
-public class ChatRequest
-        extends ChatUser {
+public class ChatRequest extends ChatUser {
     public ChatRequest(GameProfile gameProfile) {
-        super(gameProfile, UserStatus.OFFLINE, "", null, 0, System.currentTimeMillis(), 0L, "", 0L, 0L, 0, false);
+        super(gameProfile, UserStatus.OFFLINE, "", null, 0, "", 0L, 0L, 0, false);
     }
 }
 
